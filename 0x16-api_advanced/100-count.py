8c52a44d386737ac785d5de40f093694a6240bb7
tess61 <111118596+tess61@users.noreply.github.com>
2023-05-11 18:57:47 +0300

    2nd time Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,61 +1,42 @@
 #!/usr/bin/python3
-""" Count it! """
-from requests import get
-
-REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'my-app/0.0.1'}
+"""contains a function that parses the title of all hot articles,
+and prints a sorted count of given keywords"""
+import requests
 
 
-def count_words(subreddit, word_list, after="", word_dic={}):
-    """
-    Returns a list containing the titles of all hot articles for a
-    given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-    """
-    if not word_dic:
-        for word in word_list:
-            word_dic[word] = 0
-
-    if after is None:
-        word_list = [[key, value] for key, value in word_dic.items()]
-        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
-        for w in word_list:
-            if w[1]:
-                print("{}: {}".format(w[0].lower(), w[1]))
-        return None
-
-    url = REDDIT + "r/{}/hot/.json".format(subreddit)
-
-    params = {
-        'limit': 100,
-        'after': after
+def recurse(subreddit, hot_list=[], after=''):
+    """returns top tens"""
+    headers = {
+        'User-agent': 'cli:api_advanced:v1.0.0 (by /u/dereje77'
     }
+    if after is None:
+        return hot_list
 
-    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
-
+    r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     params={'limit': 100, 'after': after},
+                     headers=headers,
+                     allow_redirects=False)
     if r.status_code != 200:
         return None
+    json = r.json()
+    for post in json['data']['children']:
+        hot_list.append(post['data']['title'])
+    return recurse(subreddit, hot_list, json['data']['after'])
 
-    try:
-        js = r.json()
 
-    except ValueError:
-        return None
-
-    try:
-
-        data = js.get("data")
-        after = data.get("after")
-        children = data.get("children")
-        for child in children:
-            post = child.get("data")
-            title = post.get("title")
-            lower = [s.lower() for s in title.split(' ')]
-
-            for w in word_list:
-                word_dic[w] += lower.count(w.lower())
-
-    except:
-        return None
-
-    count_words(subreddit, word_list, after, word_dic)
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count
+    of given keywords"""
+    word_list = list(map(str.lower, word_list))
+    result = {}
+    titles = recurse(subreddit)
+    for title in titles:
+        for word in word_list:
+            if word in title:
+                if result.get(word) is None:
+                    result[word] = 0
+                result[word] += 1
+    result = [(k, v) for k, v in result.items()]
+    result.sort(key=lambda x: x[1], reverse=True)
+    for k, v in result:
+        print('{}: {}'.format(k, v))
